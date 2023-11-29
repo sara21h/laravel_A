87c5b24073931549ca0f130b50592b82380b09d6
«sara21h» <sbogdan@iesebre.com>
2023-11-29 19:11:04 +0100

    Refactoritzar la vista tasks.

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -39,10 +39,10 @@
                                         <tbody class="divide-y divide-gray-200">
                                         @foreach ($tasks as $task)
                                         <tr>
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0"><?=$task->id;?></td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?=$task->title;?></td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?=$task->description;?></td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?=$task->completed;?></td>
+                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{ $task->id }}</td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $task->title }}</td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $task->description }}</td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $task->completed }}</td>
                                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                                                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                                             </td>
